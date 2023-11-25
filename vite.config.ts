04280355ba0dd06da77957dca37c438b419e0b25
RomaLetodiani <roman.letodiani03@gmail.com>
2023-11-26 02:12:07 +0400

    finish vol 2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/interactive-rating-component',
   plugins: [react()],
   css: {
     preprocessorOptions: {
